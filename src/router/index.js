b86e87e6c6be2498af0036ccbc6ed4f10c5dd1aa
Matthieu <chevreux.matthieu@hotmail.com>
2020-08-29 15:59:19 +0200

    :fire: deleted about view and routes

--- src/router/index.js
+++ src/router/index.js
@@ -29,19 +29,6 @@ const routes = [
     }
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-    beforeEnter: checkAccessRights,
-    meta: {
-      requireAuth: true
-    }
-  },
-  {
     path: "/unauthorized",
     name: "Unauthorized",
     component: Unauthorized,
